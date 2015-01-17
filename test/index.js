f549d0970df63d322d24c09329de2dca35424908
ghaiklor <ghaiklor@gmail.com>
2015-01-17 15:50:27 +0200

    Add test for hash factory

---
+++ test/index.js
@@ -0,0 +1,17 @@
+var assert = require('assert'),
+    BitwiseHash = require('./../lib/BitwiseHash'),
+    ShortHash = require('./../lib/ShortHash'),
+    HashFactory = require('./../index'),
+    hashFactory = new HashFactory();
+
+describe('HashFactory', function () {
+    it('Should properly create bitwise hash', function () {
+        var hash = hashFactory.create('bitwise', 'some data');
+        assert(hash instanceof BitwiseHash, 'Should be instanceof BitwiseHash');
+    });
+
+    it('Should properly create short hash', function () {
+        var hash = hashFactory.create('short', 'some data');
+        assert(hash instanceof ShortHash, 'Should be instanceof ShortHash');
+    });
+});
