0fafcdaa11de3141c11d74ff0803e5737100b3c1
ghaiklor <ghaiklor@gmail.com>
2015-01-19 23:54:47 +0200

    Add print list of all hashes

--- bin/d2h.js
+++ bin/d2h.js
@@ -7,6 +7,13 @@ var program = require('commander'),
 program
     .version(pkg.version)
     .usage("<hash-function> <data>")
+    .option('-l, --list', 'Print all available hashing functions')
     .parse(process.argv);
 
-console.log(new Hash(program.args[0], program.args[1]).digest());
+if (!!program.list) {
+    Hash.SUPPORTED_HASHES.forEach(function (hash) {
+        console.log(hash);
+    });
+} else {
+    console.log(new Hash(program.args[0], program.args[1]).digest());
+}
