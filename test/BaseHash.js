c7b0718fa567776d3025714998b10fac36a4ea32
ghaiklor <ghaiklor@gmail.com>
2015-01-19 12:02:13 +0200

    Typo fixes

--- test/BaseHash.js
+++ test/BaseHash.js
@@ -3,20 +3,12 @@ var assert = require('assert'),
 
 describe('BaseHash', function () {
     it('Should create base instance', function () {
-        var hash = new BaseHash('some data');
+        var hash = new BaseHash();
         assert(hash instanceof BaseHash, 'Should be instance of BaseHash');
     });
 
-    it('Should create base instance with params', function () {
-        var hash = new BaseHash('some data');
+    it('Should properly update hash', function () {
+        var hash = new BaseHash().update('some data');
         assert.equal(hash._data, 'some data', 'Should properly set data');
-    });
-
-    it('Should properly get/set data', function () {
-        var hash = new BaseHash('some data');
-        assert.equal(hash.getData(), 'some data', 'Should properly get data');
-
-        hash.setData('another data');
-        assert.equal(hash.getData(), 'another data', 'Should properly set data');
     });
 });
