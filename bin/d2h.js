6b2e2c7bbbd348800a591947f71d2103c26d4150
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-08 13:19:36 +0200

    Small improvement in bin

--- bin/d2h.js
+++ bin/d2h.js
@@ -10,17 +10,23 @@ program
     .usage('<hash-function> <data> [options]')
     .option('-l, --list', 'Print all available hash functions')
     .option('-f, --file', 'Execute hashing of file')
-    .option('-t, --text', 'Execute hashing of text')
     .parse(process.argv);
 
 if (!!program.list) {
-    Hash.SUPPORTED_HASHES.forEach(function (hash) {
-        console.log(chalk.yellow(hash) + ' - ' + chalk.blue('d2h ' + hash + ' <data>') + ' | ' + chalk.blue('d2h ' + hash + ' <file> --file'));
-    });
+    for (var i = 0; i < Hash.SUPPORTED_HASHES.length; i++) {
+        var hash = Hash.SUPPORTED_HASHES[i];
+        process.stdout.write(chalk.yellow(hash) + ' -- ' + chalk.blue('d2h ' + hash + ' <data>') + ' || ' + chalk.blue('d2h ' + hash + ' <file> --file\n'));
+    }
+
+    process.exit(0);
 } else if (!!program.file) {
-    new Hash(program.args[0], program.args[1], true).on('done', function (hash) {
-        console.log(hash);
+    process.stdout.write('Calculating...\n');
+
+    return new Hash(program.args[0], program.args[1], true).on('done', function (hash) {
+        process.stdout.write(hash + '\n');
+        process.exit(0);
     });
 } else {
-    console.log(new Hash(program.args[0g], program.args[1]).digest());
+    process.stdout.write(new Hash(program.args[0], program.args[1]).digest() + '\n');
+    process.exit(0);
 }
